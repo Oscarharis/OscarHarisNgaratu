2e3a34f45ea33851421151afbc56384ae00f6c80
Oscarharis <147423427+Oscarharis@users.noreply.github.com>
2024-03-04 23:25:20 +0700

    Add files via upload

---
+++ Biodata Oscar Haris Ngaratu.py
@@ -0,0 +1,10 @@
+nama = "Oscar Haris Ngaratu"
+kelas = "TI23B"
+jurusan = "TEKNIK INFORMATIKA"
+quote = "Belajarlah dengan giat,agar masa depanmu lebih cerah"
+
+print("##biodata")
+print("nama:", nama)
+print("kelas:", kelas)
+print("jurusan:", jurusan)
+print("quote:", quote)
